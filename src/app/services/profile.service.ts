d488fb25ce488f8f5be9e384767be937d1ee0533
Safwan <safwan9f@gmail.com>
2020-08-25 00:37:28 +0300

    Get the desing for the profile component

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -16,6 +16,26 @@ export class ProfileService {
       'Safwan Saigh',
       '../assets/images/me.jpg'
     ),
+    new Profile(
+      'safwan9f@gmail.com',
+      'Safwan Saigh',
+      '../assets/images/me.jpg'
+    ),
+    new Profile(
+      'safwan9f@gmail.com',
+      'Safwan Saigh',
+      '../assets/images/me.jpg'
+    ),
+    new Profile(
+      'safwan9f@gmail.com',
+      'Safwan Saigh',
+      '../assets/images/me.jpg'
+    ),
+    new Profile(
+      'safwan9f@gmail.com',
+      'Safwan Saigh',
+      '../assets/images/me.jpg'
+    ),
   ];
   constructor() {}
 }
